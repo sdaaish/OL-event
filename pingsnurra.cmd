c419c9f340c85ed6012f94bf8b7e68decb21a98c
Stig Dahl <Stig@charlottendal.net>
2016-09-03 22:17:48 +0200

    Changed comments from rem to ::. Posh edits...

--- pingsnurra.cmd
+++ pingsnurra.cmd
@@ -1,18 +1,18 @@
 @echo off
 
-rem Pingsnurra för datorer på OL,tävling
-rem 2016-08-20/SDAA
+:: Pingsnurra för datorer på OL,tävling
+:: 2016-08-20/SDAA
 
-rem Vi börjar om här
+:: Vi börjar om här
 :begin
 
-rem Set date
+:: Set date
 set $Day=%Date:~8,2%
 set $Mth=%Date:~5,2%
 set $Year=%Date:~0,4%
 set $Datum=%$Year%-%$Mth%-%$Day%
 
-rem Set time
+:: Set time
 set $Timma=%Time:~0,2%
 set $Minut=%Time:~3,2%
 set $Sekund=%Time:~6,2%
@@ -22,14 +22,14 @@ cls
 echo > NUL
 echo Aktuellt datum: %$Datum% och tid: %$Tid%
 
-rem pingar Gof-datorer i nätet
-rem Kolla att localhost och gateway fungerar
+:: pingar Gof-datorer i nätet
+:: Kolla att localhost och gateway fungerar
 for %%i in (localhost 192.168.1.1) do ping -n 3 -w 300 -l 128 %%i| findstr /i /v  "statistics approx minimum"
 
-rem Kolla de olika Gofarna
+:: Kolla de olika Gofarna
 for /L %%i in (11,1,17) do ping -n 3 -w 300 -l 128 192.168.1.%%i| findstr /i /v  "statistics approx minimum"
 
-rem Börja om igen, låt detta fönster snurra
+:: Börja om igen, låt detta fönster snurra
 echo > NUL
 echo Done!
 echo Restarting ping, sleep for a while.....
