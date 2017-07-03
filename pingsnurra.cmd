e1c454836a221d0b88963c9bd73bfc0a86997c00
Stig Dahl <Stig@charlottendal.net>
2017-07-03 19:00:08 +0200

    Added an infile to the cmd-file pingsnurra.

--- pingsnurra.cmd
+++ pingsnurra.cmd
@@ -1,9 +1,10 @@
 @echo off
 
-:: Pingsnurra för datorer på OL,tävling
+:: Ping tool to check if computers on OL-event are responding in the network.
+:: Must supply a infile with ip-addresses as infile.
 :: 2016-08-20/SDAA
 
-:: Vi börjar om här
+:: Start over from here
 :begin
 
 :: Set date
@@ -13,38 +14,54 @@ set $Year=%Date:~0,4%
 set $Datum=%$Year%-%$Mth%-%$Day%
 
 :: Set time
-set $Timma=%Time:~0,2%
-set $Minut=%Time:~3,2%
-set $Sekund=%Time:~6,2%
-set $Tid=%$Timma%:%$Minut%:%$Sekund%
+set $Hour=%Time:~0,2%
+set $Minute=%Time:~3,2%
+set $Second=%Time:~6,2%
+set $Tid=%$Hour%:%$Minute%:%$Second%
 
+:: Must supply infile-name
+if [%1]==[] goto usage
+
+:: Check for infile
+if not exist %1 (
+   echo File %1 does not exist!
+   exit /b 1
+)
+
+:: This file contains the addresses, see supplied example.txt
+set $infile=%1
+
+:: Clear screen before pining
 cls
 echo > NUL
-echo Aktuellt datum: %$Datum% och tid: %$Tid%
+echo Current date: %$Datum% and time: %$Tid%
 
-:: pingar Gof-datorer i nätet
-:: Kolla att localhost och gateway fungerar
-for %%i in (localhost 192.168.1.1) do (
+:: Check localhost
+for %%i in (localhost) do (
     echo Pinging host=%%i
     ping -n 3 -w 300 -l 128 %%i| findstr /i /v  "statistics approx minimum"
 )
 
 setlocal
-:: Kolla de olika Gofarna
-for /L %%i in (11,1,17) do (call :pingloop %%i)
+:: Check the computers in $infile
+for /f "eol=#" %%i in (%$infile%) do (call :pingloop %%i)
 endlocal
 goto rerun
 
 :pingloop
-set $host=192.168.1.%1
+set $host=%1
 echo Pinging host %$host%
 ping -n 3 -w 300 -l 128 %$host% | findstr /i /v  "statistics approx minimum"
 exit /b
 
 :rerun
-:: Börja om igen, låt detta fönster snurra
+:: Start over after 5 secs.
 echo > NUL
 echo Done!
 echo Restarting ping, sleep for a while.....
 ping -n 5 ::1 > NUL
 goto begin
+
+:usage
+echo "Usage: pingsnurra.cmd <filename>"
+exit /b 2
