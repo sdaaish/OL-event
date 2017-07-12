779f0839e78c76f79671ace1ca3b8e74f8a444bb
Stig Dahl <Stig@charlottendal.net>
2017-07-12 22:13:25 +0200

    Added mysqldb-restore.cmd.

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 @echo off
 
 echo builds a release
-7z a .\OL-event.zip check-roc.ps1 example-infile.txt mysqldb-backup.cmd ntp.conf pingloop.cmd LICENSE
+7z a .\OL-event.zip check-roc.ps1 example-infile.txt mysqldb-backup.cmd mysqldb-restore.cmd ntp.conf pingloop.cmd LICENSE
